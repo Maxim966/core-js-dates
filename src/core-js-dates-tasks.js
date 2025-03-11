adddc9b2155e697e786ad365d1e1b6f192116278
Maksim Sukhov <silveraven@yandex.ru>
2025-03-11 23:40:01 +0200

    feat: add solution getNextFriday

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -74,8 +74,16 @@ function getDayName(date) {
  * Date('2024-02-13T00:00:00Z') => Date('2024-02-16T00:00:00Z')
  * Date('2024-02-16T00:00:00Z') => Date('2024-02-23T00:00:00Z')
  */
-function getNextFriday(/* date */) {
-  throw new Error('Not implemented');
+function getNextFriday(date) {
+  const dayOfWeek = date.getDay();
+  let dayToFriday;
+  if (dayOfWeek === 5) {
+    dayToFriday = 7;
+  } else {
+    dayToFriday = (7 + 5 - dayOfWeek) % 7;
+  }
+  date.setDate(date.getDate() + dayToFriday);
+  return date;
 }
 
 /**
